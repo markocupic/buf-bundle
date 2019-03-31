4312947e7c1970d250193b18dab25b633ea533dc
Marko Cupic <m.cupic@gmx.ch>
2019-03-31 10:06:50 +0200

    initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Markocupic\BufBundle\ContaoManager;
+
+use Markocupic\BufBundle\MarkocupicBufBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(MarkocupicBufBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['buf']),
+        ];
+    }
+}
